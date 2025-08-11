252b54d528f38dbce6cab43896b78e09603cac36
NH-Riyan <nazmulhasanriyan@gmail.com>
2025-08-12 01:06:19 +0600

    added get , patch

--- index.js
+++ index.js
@@ -1,56 +1,87 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
-const app=express();
-const port=process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster10.oop5ill.mongodb.net/?retryWrites=true&w=majority&appName=Cluster10`;
 
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-app.get('/',(req,res)=>{
-    res.send('hellou')
+app.get('/', (req, res) => {
+  res.send('hellou')
 })
 
-app.listen(port,()=>
-  {
-      console.log(`port is running ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`port is running ${port}`)
+})
 
-  async function run() {
-    try {
-        const CourseList = client.db("A11B11").collection('Courses')
-      
-      await client.connect();
+async function run() {
+  try {
+    const CourseList = client.db("A11B11").collection('Courses')
 
-      app.get('/courses',async(req,res)=>{
-        const List = CourseList.find()
-        const result=await List.toArray()
-        res.send(result)
-      })
+    await client.connect();
 
-      app.post('/addcourses',async(req,res)=>
-        {
-            const newCourse = req.body;
-            const result = await CourseList.insertOne(newCourse)
-            res.send(result)
-        })
-     
-     // await client.db("admin").command({ ping: 1 });
-     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-     
-      //await client.close();
-    }
+    app.get('/courses', async (req, res) => {
+      const List = CourseList.find()
+      const result = await List.toArray()
+      res.send(result)
+    })
+
+    app.get('/courses/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await CourseList.findOne(query)
+      res.send(result)
+    })
+
+    app.get('/courses/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const result = await CourseList.find({ email }).toArray();
+      res.send(result);
+    })
+
+    app.post('/addcourses', async (req, res) => {
+      const newCourse = req.body;
+      const result = await CourseList.insertOne(newCourse)
+      res.send(result)
+    })
+
+
+
+    app.patch('/courses/:id',async(req,res)=>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateddata = req.body;
+      const update = { $set: updateddata};
+
+      const result= await CourseList.updateOne(filter, update)
+      res.send(result)
+    })
+
+    app.delete('/courses/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await CourseList.deleteOne(query)
+      res.send(result)
+
+    })
+
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    //await client.close();
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
